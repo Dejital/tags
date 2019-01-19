df20feb99d4903a8d1016000724b2119232a2610
Serge Nevsky <serge@snevsky.com>
2019-01-19 09:09:01 -0600

    Mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const tags = [
+  "foo", "bar", "foobar"
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <h1>Instagram Tag Generator</h1>
+        <TagGenerator />
+        <footer>
+          <a href="http://snevsky.com">
+            <p>App by Serge Nevsky</p>
           </a>
-        </header>
+        </footer>
       </div>
     );
   }
+}
+
+class TagGenerator extends Component {
+  state = {
+    output: ""
+  }
+  componentDidMount = () => this.setRandomTags();
+  render() {
+    return (
+      <div>
+        <button onClick={this.setRandomTags.bind(this)}>Generate</button>
+        <br />
+        <br />
+        <textarea value={this.state.output} />
+      </div>
+    );
+  }
+  private setRandomTags = () => this.setState({ output: getRandomTags() });
+}
+
+function getRandomTags()
+{
+  return ".\n.\n.\n.\n.\n#foo #bar #foobar";
 }
 
 export default App;
