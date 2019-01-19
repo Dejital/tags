58294763d42a6ae7df80d8a6c31104e7f8bf5785
Serge Nevsky <serge@snevsky.com>
2019-01-19 10:32:47 -0600

    Updates

---
+++ precache-manifest.daa85ce671e3c3aa7433c3e094b0ef67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "712fb53b6e979bd45b12",
+    "url": "/tags/static/js/runtime~main.712fb53b.js"
+  },
+  {
+    "revision": "fadb48695772999e12b4",
+    "url": "/tags/static/js/main.fadb4869.chunk.js"
+  },
+  {
+    "revision": "c91c4ddbcb81f170faf4",
+    "url": "/tags/static/js/1.c91c4ddb.chunk.js"
+  },
+  {
+    "revision": "fadb48695772999e12b4",
+    "url": "/tags/static/css/main.6f9263f6.chunk.css"
+  },
+  {
+    "revision": "cc913bc52ad159abbc167509f7d44f10",
+    "url": "/tags/index.html"
+  }
+];
